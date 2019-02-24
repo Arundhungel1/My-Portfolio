9b160903f2723779c89e1808f8309a1632b282ae
Arun Dhungel <arundhungel.npa@gmail.com>
2019-02-24 12:55:21 +1100

    adding portfolio section

--- js/script.js
+++ js/script.js
@@ -28,10 +28,38 @@ $(document).ready(function() {
         }
       }
     })
-    $(".counter").each(function () {
-      let element = $(this);
-      let endVal = parseInt(element.text());
 
-      element.countup(endVal);
-      })
-  })
+        $(".counter").each(function () {
+          let element = $(this);
+          let endVal = parseInt(element.text());
+
+          element.countup(endVal);
+      });
+
+      $("[data-fancybox]").fancybox();
+
+      $(".items").isotope({
+        filter: "*",
+        animationOptions: {
+          duration: 1500,
+          easing: 'linear',
+          queue: false
+        }
+        });
+$("#filters a").click(function() {
+  $("#filters .current").removeClass("current");
+  $(this).addClass("current");
+
+  let selector = $(this).attr("data-filter");
+
+  $(".items").isotope({
+    filter: selector,
+    animationOptions: {
+      duration: 1500,
+      easing: 'linear',
+      queue: false,
+    }
+    });
+    return false;
+  });
+  });
